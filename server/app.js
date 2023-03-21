ee37c87af07dbda2b91a83396dfca443c6c2c7bc
Yevhenii Danoi <geneforapp@gmail.com>
2023-03-21 19:56:51 +0200

    changes

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.resolve(__dirname, "../client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build", "index.hmtl"));
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => res.send("Please set tot production"));
